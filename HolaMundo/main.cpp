012a00305119bdce6d524803ed48b7d7fcc86271
sinoe9891 <sinoeproducciones@gmail.com>
2020-10-16 00:04:02 -0600

    Curso Introducción C++

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "          . " << endl;
+    cout << " .>   )\;`a__" << endl;
+    cout << "(  _ _)/ /-. ~~" << endl;
+    cout << " `( )_ )/" << endl;
+    cout << "  <_  <_" << endl;
+
+    cout << 10 << endl;
+    cout << 3.1416f;
+    cout << true << endl;
+    cout << 'c' << endl;
+
+    cout << "Hola Platzi" << endl;
+
+//  Alojando Variables
+    int month = 12;
+    int day = 4;
+
+    day = month;
+
+    cout << month << endl;
+    cout << day << endl;
+
+    float pi = 3.1416;
+
+    cout << pi << endl;
+
+    bool isGameOver = false;
+
+    cout << "El jugador ya perdio" + isGameOver<< endl;
+
+    return 0;
+}
